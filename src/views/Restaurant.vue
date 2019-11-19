7a79788adb70d5d4a253e2d76d4f2ed15f8007de
walle <michelle210181@gmail.com>
2019-11-19 15:14:31 +0800

    add restaurant detail page

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container py-5">
     <h1>餐廳描述頁</h1>
-    <!-- 餐廳資訊頁 RestaurantDetail -->
+    <RestaurantDetail :initial-restaurant="restaurant" />
     <hr />
     <!-- 餐廳評論 RestaurantComments -->
     <!-- 新增評論 CreateComment -->
@@ -9,6 +9,8 @@
 </template>
 
 <script>
+import RestaurantDetail from "./../components/RestaurantDetail";
+
 const dummyData = {
   restaurant: {
     id: 1,
@@ -114,6 +116,9 @@ const dummyData = {
 };
 
 export default {
+  components: {
+    RestaurantDetail
+  },
   data() {
     return {
       restaurant: {
