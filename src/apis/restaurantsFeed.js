ce8c1a3aa6b941331ed27ab5ba8a4e34341a3d7f
walle <michelle210181@gmail.com>
2019-11-23 21:47:20 +0800

    revise restaurant feeds page

---
+++ src/apis/restaurantsFeed.js
@@ -0,0 +1,10 @@
+import { apiHelper } from './../utils/helpers'
+const getToken = () => localStorage.getItem('token')
+
+export default {
+  getRestaurantsFeed() {
+    return apiHelper.get(`/restaurants/feeds`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  }
+}
--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -15,256 +15,12 @@
 </template>
 
 <script>
-import NavTabs from "./../components/NavTabs";
-import NewestRestaurants from "./../components/NewestRestaurants";
-import NewestComments from "./../components/NewestComments";
-const dummyData = {
-  restaurants: [
-    {
-      id: 67,
-      name: "1681111",
-      tel: "",
-      address: "",
-      opening_hours: "",
-      description: "",
-      image: null,
-      viewCounts: null,
-      createdAt: "2019-09-15T11:23:22.460Z",
-      updatedAt: "2019-09-15T11:33:49.998Z",
-      CategoryId: 4,
-      Category: {
-        id: 4,
-        name: "墨西哥料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      }
-    },
-    {
-      id: 47,
-      name: "Beverly Yundt",
-      tel: "099-398-9865 x7776",
-      address: "27001 Ike Well",
-      opening_hours: "08:00",
-      description:
-        "Quibusdam ut natus officia porro. Quis eum explicabo architecto sint repudiandae unde. Sunt magni quibusdam ipsa et impedit esse occaecati. Et nemo quaerat saepe delectus. Autem dignissimos corporis sed voluptates suscipit quia fuga. Dolorum ea suscipit.",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=44.588878866397906",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.443Z",
-      updatedAt: "2019-07-30T16:24:55.443Z",
-      CategoryId: 5,
-      Category: {
-        id: 5,
-        name: "素食料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      }
-    },
-    {
-      id: 49,
-      name: "Russ Kertzmann",
-      tel: "087-274-1881",
-      address: "6601 Lura Lodge",
-      opening_hours: "08:00",
-      description: "sed",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=56.98329738383367",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.443Z",
-      updatedAt: "2019-07-30T16:24:55.443Z",
-      CategoryId: 4,
-      Category: {
-        id: 4,
-        name: "墨西哥料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      }
-    },
-    {
-      id: 46,
-      name: "Jasper Mueller",
-      tel: "254-159-2190",
-      address: "44890 Dominique Wall",
-      opening_hours: "08:00",
-      description: "dolorem ratione et",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=67.08831513935154",
-      viewCounts: 37,
-      createdAt: "2019-07-30T16:24:55.443Z",
-      updatedAt: "2019-09-15T08:50:04.423Z",
-      CategoryId: 2,
-      Category: {
-        id: 2,
-        name: "日本料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-09-26T15:11:56.408Z"
-      }
-    },
-    {
-      id: 50,
-      name: "Mrs. Wellington Lehner",
-      tel: "792-886-2864 x53152",
-      address: "4185 Cartwright Green",
-      opening_hours: "08:00",
-      description: "aspernatur",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=56.977654953966386",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.443Z",
-      updatedAt: "2019-07-30T16:24:55.443Z",
-      CategoryId: 3,
-      Category: {
-        id: 3,
-        name: "義大利料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      }
-    },
-    {
-      id: 48,
-      name: "Lucie McDermott",
-      tel: "(087) 019-9323 x4728",
-      address: "218 Bennett Street",
-      opening_hours: "08:00",
-      description: "omnis",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=47.899925023303915",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.443Z",
-      updatedAt: "2019-07-30T16:24:55.443Z",
-      CategoryId: 4,
-      Category: {
-        id: 4,
-        name: "墨西哥料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      }
-    },
-    {
-      id: 43,
-      name: "Carrie Robel V",
-      tel: "455.357.0399 x356",
-      address: "33531 Mayert Fall",
-      opening_hours: "08:00",
-      description:
-        "Quos illum dolorem laboriosam temporibus incidunt non nihil.\nPerspiciatis iusto cumque possimus.",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=41.74175407974987",
-      viewCounts: 13,
-      createdAt: "2019-07-30T16:24:55.442Z",
-      updatedAt: "2019-10-03T09:57:48.018Z",
-      CategoryId: 5,
-      Category: {
-        id: 5,
-        name: "素食料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      }
-    },
-    {
-      id: 42,
-      name: "Melody Koepp",
-      tel: "(774) 342-2533",
-      address: "3318 Feeney Hollow",
-      opening_hours: "08:00",
-      description: "beatae iste alias",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=71.34426799459621",
-      viewCounts: 12,
-      createdAt: "2019-07-30T16:24:55.442Z",
-      updatedAt: "2019-09-15T08:47:06.852Z",
-      CategoryId: 2,
-      Category: {
-        id: 2,
-        name: "日本料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-09-26T15:11:56.408Z"
-      }
-    },
-    {
-      id: 44,
-      name: "Sonny Funk",
-      tel: "093-689-0372",
-      address: "908 Kassulke Landing",
-      opening_hours: "08:00",
-      description:
-        "Asperiores sequi doloribus. Alias corporis praesentium quas amet. Ad et delectus amet ea reiciendis. Perspiciatis aliquid sapiente doloremque. Ut hic reiciendis velit voluptates atque.",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=88.97383668164223",
-      viewCounts: 2,
-      createdAt: "2019-07-30T16:24:55.442Z",
-      updatedAt: "2019-10-03T09:30:23.290Z",
-      CategoryId: 2,
-      Category: {
-        id: 2,
-        name: "日本料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-09-26T15:11:56.408Z"
-      }
-    },
-    {
-      id: 45,
-      name: "Luther Hickle",
-      tel: "477-667-6528 x320",
-      address: "79381 Bradtke Manor",
-      opening_hours: "08:00",
-      description:
-        "Esse qui rerum et occaecati quia quo molestiae. Consequuntur rem iste doloribus minus esse ea voluptas atque. Dolore distinctio tempore expedita atque unde aliquam laudantium.\n \rQuaerat est ut. Nulla dolore a error eaque voluptatem quo. Laborum quis reiciendis in ipsa dolores dolor exercitationem itaque deserunt. Rerum ut ab sequi consequatur quae nulla. Consequatur nam aut voluptate amet qui omnis. Voluptatem commodi assumenda quisquam consequatur magni quod repellat ut dolore.\n \rRem nesciunt quia adipisci debitis distinctio est. Odit voluptas autem repellendus commodi a quas voluptatum. At quia modi ut ea earum consequatur facere recusandae.",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=78.10379900896436",
-      viewCounts: 1,
-      createdAt: "2019-07-30T16:24:55.442Z",
-      updatedAt: "2019-09-09T14:20:54.212Z",
-      CategoryId: 3,
-      Category: {
-        id: 3,
-        name: "義大利料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      }
-    }
-  ],
-  comments: [
-    {
-      id: 172,
-      text: "test 1",
-      UserId: 1,
-      RestaurantId: 67,
-      createdAt: "2019-10-03T10:43:55.617Z",
-      updatedAt: "2019-10-03T10:43:55.617Z",
-      User: {
-        id: 1,
-        name: "root test",
-        email: "root@example.com",
-        password:
-          "$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe",
-        isAdmin: true,
-        image: "https://i.imgur.com/58ImzMM.png",
-        createdAt: "2019-07-30T16:24:54.983Z",
-        updatedAt: "2019-09-15T07:49:43.737Z"
-      },
-      Restaurant: {
-        id: 67,
-        name: "1681111",
-        tel: "",
-        address: "",
-        opening_hours: "",
-        description: "",
-        image: null,
-        viewCounts: null,
-        createdAt: "2019-09-15T11:23:22.460Z",
-        updatedAt: "2019-09-15T11:33:49.998Z",
-        CategoryId: 4,
-        Category: {
-          id: 4,
-          name: "墨西哥料理",
-          createdAt: "2019-07-30T16:24:55.429Z",
-          updatedAt: "2019-07-30T16:24:55.429Z"
-        }
-      }
-    }
-  ]
-};
+import NavTabs from './../components/NavTabs'
+import NewestRestaurants from './../components/NewestRestaurants'
+import NewestComments from './../components/NewestComments'
+import restaurantsFeedAPI from './../apis/restaurantsFeed'
+import { Toast } from './../utils/helpers'
+
 export default {
   components: {
     NavTabs,
@@ -275,16 +31,30 @@ export default {
     return {
       restaurants: [],
       comments: []
-    };
+    }
   },
   created() {
-    this.fetchFeeds();
+    this.fetchFeeds()
   },
   methods: {
-    fetchFeeds() {
-      this.restaurants = dummyData.restaurants;
-      this.comments = dummyData.comments;
+    async fetchFeeds() {
+      try {
+        const response = await restaurantsFeedAPI.getRestaurantsFeed()
+        const { data, statusText } = response
+
+        if (statusText !== 'OK') {
+          throw new Error(statusText)
+        }
+
+        this.restaurants = data.restaurants
+        this.comments = data.comments
+      } catch {
+        Toast.fire({
+          type: 'error',
+          title: '無法取得資料，請稍後再試'
+        })
+      }
     }
   }
-};
+}
 </script>
