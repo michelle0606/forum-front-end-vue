a21168d80b0b6fce7a311132d8c1191eac3d8d83
walle <michelle210181@gmail.com>
2019-11-23 20:27:06 +0800

    use real data from server in restaurants page

--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -19,290 +19,14 @@
 </template>
 
 <script>
-import NavTabs from "./../components/NavTabs";
-import RestaurantCard from "./../components/RestaurantCard";
-import RestaurantsNavPills from "./../components/RestaurantsNavPills";
-import RestaurantsPagination from "./../components/RestaurantsPagination";
+import NavTabs from './../components/NavTabs'
+import RestaurantCard from './../components/RestaurantCard'
+import RestaurantsNavPills from './../components/RestaurantsNavPills'
+import RestaurantsPagination from './../components/RestaurantsPagination'
+import restaurantsAPI from './../apis/restaurants'
 
-const dummyData = {
-  restaurants: [
-    {
-      id: 11,
-      name: "Haley Trantow",
-      tel: "(919) 824-7063 x83066",
-      address: "86902 Yost Parks",
-      opening_hours: "08:00",
-      description: "Nam ipsum magnam quia.\r\nCulpa voluptate ducimus un",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=45.89406888977074",
-      viewCounts: 3,
-      createdAt: "2019-07-30T16:24:55.435Z",
-      updatedAt: "2019-10-07T09:04:42.057Z",
-      CategoryId: 5,
-      Category: {
-        id: 5,
-        name: "素食料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 21,
-      name: "Clovis Mayert",
-      tel: "1-292-930-4113 x0577",
-      address: "3255 Streich Inlet",
-      opening_hours: "08:00",
-      description: "Nihil occaecati consectetur explicabo sint nobis b",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=31.579240610126313",
-      viewCounts: 6,
-      createdAt: "2019-07-30T16:24:55.436Z",
-      updatedAt: "2019-08-04T17:14:28.400Z",
-      CategoryId: 3,
-      Category: {
-        id: 3,
-        name: "義大利料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 13,
-      name: "Curt Corkery",
-      tel: "377-916-6821 x9397",
-      address: "3757 Alfonzo Corner",
-      opening_hours: "08:00",
-      description: "enim",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=19.39164947251193",
-      viewCounts: 1,
-      createdAt: "2019-07-30T16:24:55.435Z",
-      updatedAt: "2019-10-03T09:25:54.492Z",
-      CategoryId: 2,
-      Category: {
-        id: 2,
-        name: "日本料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-09-26T15:11:56.408Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 6,
-      name: "Clair Herzog",
-      tel: "868-946-9602 x92653",
-      address: "65909 Marcelo Lock",
-      opening_hours: "08:00",
-      description: "Magni enim similique et doloribus fugit.",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=67.15023188933482",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.434Z",
-      updatedAt: "2019-08-25T07:50:39.498Z",
-      CategoryId: 5,
-      Category: {
-        id: 5,
-        name: "素食料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 9,
-      name: "Susan Johnston",
-      tel: "260-837-0348 x3437",
-      address: "0802 Tillman Crossing",
-      opening_hours: "08:00",
-      description: "Aspernatur aliquam totam pariatur ut mollitia odio",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=41.270646115002016",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.434Z",
-      updatedAt: "2019-08-25T07:50:57.817Z",
-      CategoryId: 3,
-      Category: {
-        id: 3,
-        name: "義大利料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 4,
-      name: "Odessa Larkin",
-      tel: "516-154-3810 x11365",
-      address: "1044 Dayana Gardens",
-      opening_hours: "08:00",
-      description: "Similique eum laborum molestiae. Temporibus natus ",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=74.38474595344819",
-      viewCounts: 1,
-      createdAt: "2019-07-30T16:24:55.433Z",
-      updatedAt: "2019-09-09T14:18:42.390Z",
-      CategoryId: 2,
-      Category: {
-        id: 2,
-        name: "日本料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-09-26T15:11:56.408Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 10,
-      name: "Howell Towne",
-      tel: "(221) 302-7497 x511",
-      address: "0858 Koss Via",
-      opening_hours: "08:00",
-      description: "laboriosam",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=54.054925465974634",
-      viewCounts: 2,
-      createdAt: "2019-07-30T16:24:55.434Z",
-      updatedAt: "2019-08-26T10:14:20.978Z",
-      CategoryId: 4,
-      Category: {
-        id: 4,
-        name: "墨西哥料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 19,
-      name: "Dustin Lynch",
-      tel: "1-382-742-2344 x941",
-      address: "102 Johnson Camp",
-      opening_hours: "08:00",
-      description: "Veritatis quae ad id amet. Similique est suscipit.",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=19.604747512748677",
-      viewCounts: 2,
-      createdAt: "2019-07-30T16:24:55.436Z",
-      updatedAt: "2019-09-07T16:22:46.020Z",
-      CategoryId: 5,
-      Category: {
-        id: 5,
-        name: "素食料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: true,
-      isLiked: false
-    },
-    {
-      id: 22,
-      name: "Mr. Lavonne Grimes",
-      tel: "841.933.1636",
-      address: "298 Albin Road",
-      opening_hours: "08:00",
-      description: "Quia eum eum sapiente sint distinctio eveniet.",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=64.93265026937894",
-      viewCounts: 3,
-      createdAt: "2019-07-30T16:24:55.437Z",
-      updatedAt: "2019-09-09T14:18:52.056Z",
-      CategoryId: 2,
-      Category: {
-        id: 2,
-        name: "日本料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-09-26T15:11:56.408Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 1,
-      name: "Laurence Reynolds",
-      tel: "1-657-067-3756 x9782",
-      address: "187 Kirlin Squares",
-      opening_hours: "08:00",
-      description: "sit est mollitia",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=91.29816290184887",
-      viewCounts: 36,
-      createdAt: "2019-07-30T16:24:55.432Z",
-      updatedAt: "2019-09-27T14:24:19.856Z",
-      CategoryId: 3,
-      Category: {
-        id: 3,
-        name: "義大利料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: true
-    }
-  ],
-  categories: [
-    {
-      id: 3,
-      name: "義大利料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    },
-    {
-      id: 4,
-      name: "墨西哥料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    },
-    {
-      id: 5,
-      name: "素食料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    },
-    {
-      id: 6,
-      name: "美式料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    },
-    {
-      id: 7,
-      name: "複合式料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    },
-    {
-      id: 1,
-      name: "中式料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-09-08T08:51:28.715Z"
-    },
-    {
-      id: 2,
-      name: "日本料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-09-26T15:11:56.408Z"
-    },
-    {
-      id: 31,
-      name: "新疆料理",
-      createdAt: "2019-09-26T19:58:57.874Z",
-      updatedAt: "2019-09-26T19:58:57.874Z"
-    }
-  ],
-  categoryId: "",
-  page: 1,
-  totalPage: [1, 2, 3, 4, 5],
-  prev: 1,
-  next: 2
-};
+import { Toast } from './../utils/helpers'
+
 export default {
   components: {
     NavTabs,
@@ -317,19 +41,45 @@ export default {
       currentPage: 1,
       restaurants: [],
       totalPage: -1
-    };
+    }
   },
   created() {
-    this.fetchRestaurants();
+    const { page = 1, categoryId = '' } = this.$route.query
+    this.fetchRestaurants({
+      page,
+      categoryId
+    })
+  },
+  beforeRouteUpdate(to, from, next) {
+    const { page } = to.query
+    this.fetchRestaurants({ page, categoryId: '' })
+    next()
   },
   methods: {
-    fetchRestaurants() {
-      this.categories = dummyData.categories;
-      this.categoryId = dummyData.categoryId;
-      this.currentPage = dummyData.page;
-      this.restaurants = dummyData.restaurants;
-      this.totalPage = dummyData.totalPage.length;
+    async fetchRestaurants({ page, categoryId }) {
+      try {
+        const response = await restaurantsAPI.getRestaurants({
+          page,
+          categoryId
+        })
+        const { data, statusText } = response
+
+        if (statusText !== 'OK') {
+          throw new Error(statusText)
+        }
+
+        this.categories = data.categories
+        this.categoryId = data.categoryId
+        this.currentPage = data.page
+        this.restaurants = data.restaurants
+        this.totalPage = data.totalPage.length
+      } catch (error) {
+        Toast.fire({
+          type: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
+      }
     }
   }
-};
+}
 </script>
