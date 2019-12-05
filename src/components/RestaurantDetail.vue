b002a631e9fd974394ffd3de08438b7c8bb1b903
walle <michelle210181@gmail.com>
2019-12-05 16:02:48 +0800

    use api to fetch restaurant information and use mapstate to update currentUser

--- src/components/RestaurantDetail.vue
+++ src/components/RestaurantDetail.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="row">
     <div class="col-md-12 mb-3">
-      <h1>{{restaurant.name}}</h1>
-      <p class="badge badge-secondary mt-1 mb-3">{{restaurant.categoryName}}</p>
+      <h1>{{ restaurant.name }}</h1>
+      <p class="badge badge-secondary mt-1 mb-3">
+        {{ restaurant.categoryName }}
+      </p>
     </div>
     <div class="col-lg-4">
       <img
@@ -14,15 +16,15 @@
         <ul class="list-unstyled">
           <li>
             <strong>Opening Hour:</strong>
-            {{restaurant.opening_hours}}
+            {{ restaurant.opening_hours }}
           </li>
           <li>
             <strong>Tel:</strong>
-            {{restaurant.tel}}
+            {{ restaurant.tel }}
           </li>
           <li>
             <strong>Address:</strong>
-            {{restaurant.address}}
+            {{ restaurant.address }}
           </li>
         </ul>
       </div>
@@ -36,25 +38,33 @@
         type="button"
         class="btn btn-danger btn-border mr-2"
         @click.stop.prevent="deleteFavorite"
-      >移除最愛</button>
+      >
+        移除最愛
+      </button>
       <button
         v-else
         type="button"
         class="btn btn-primary btn-border mr-2"
         @click.stop.prevent="addFavorite"
-      >加到最愛</button>
+      >
+        加到最愛
+      </button>
       <button
         v-if="restaurant.isLiked"
         type="button"
         class="btn btn-danger like mr-2"
         @click.stop.prevent="deleteLike"
-      >Unlike</button>
+      >
+        Unlike
+      </button>
       <button
         v-else
         type="button"
         class="btn btn-primary like mr-2"
         @click.stop.prevent="addLike"
-      >Like</button>
+      >
+        Like
+      </button>
     </div>
   </div>
 </template>
@@ -70,33 +80,42 @@ export default {
   data() {
     return {
       restaurant: this.initialRestaurant
-    };
+    }
+  },
+  watch: {
+    initialRestaurant(restaurant) {
+      this.restaurant = {
+        ...this.restaurant,
+        ...restaurant
+      }
+    }
   },
   methods: {
     addFavorite() {
       this.restaurant = {
         ...this.restaurant, // 保留餐廳內原有資料
         isFavorited: true
-      };
+      }
     },
     deleteFavorite() {
       this.restaurant = {
         ...this.restaurant, // 保留餐廳內原有資料
         isFavorited: false
-      };
+      }
     },
     addLike() {
       this.restaurant = {
         ...this.restaurant,
         isLiked: true
-      };
+      }
     },
     deleteLike() {
       this.restaurant = {
         ...this.restaurant,
         isLiked: false
-      };
+      }
     }
   }
-};
-</script>>
+}
+</script>
+>
--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -2,7 +2,10 @@
   <div class="container py-5">
     <RestaurantDetail :initial-restaurant="restaurant" />
     <hr />
-    <CreateComment :restaurant-id="restaurant.id" @after-create-comment="afterCreateComment" />
+    <CreateComment
+      :restaurant-id="restaurant.id"
+      @after-create-comment="afterCreateComment"
+    />
     <RestaurantComments
       :restaurant-comments="restaurantComments"
       @after-delete-comment="afterDeleteComment"
@@ -11,123 +14,13 @@
 </template>
 
 <script>
-import RestaurantDetail from "./../components/RestaurantDetail";
-import RestaurantComments from "./../components/RestaurantComments";
-import CreateComment from "./../components/CreateComment";
-CreateComment;
-const dummyUser = {
-  currentUser: {
-    id: 1,
-    name: "管理者",
-    email: "root@example.com",
-    image: "https://i.pravatar.cc/300",
-    isAdmin: true
-  },
-  isAuthenticated: true
-};
-const dummyData = {
-  restaurant: {
-    id: 1,
-    name: "Laurence Reynolds",
-    tel: "1-657-067-3756 x9782",
-    address: "187 Kirlin Squares",
-    opening_hours: "08:00",
-    description: "sit est mollitia",
-    image:
-      "https://loremflickr.com/320/240/restaurant,food/?random=91.29816290184887",
-    viewCounts: 37,
-    createdAt: "2019-07-30T16:24:55.432Z",
-    updatedAt: "2019-11-10T01:35:39.708Z",
-    CategoryId: 3,
-    Category: {
-      id: 3,
-      name: "義大利料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    },
-    FavoritedUsers: [],
-    LikedUsers: [
-      {
-        id: 1,
-        name: "root test",
-        email: "root@example.com",
-        password:
-          "$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe",
-        isAdmin: true,
-        image: "https://i.imgur.com/58ImzMM.png",
-        createdAt: "2019-07-30T16:24:54.983Z",
-        updatedAt: "2019-09-15T07:49:43.737Z",
-        Like: {
-          UserId: 1,
-          RestaurantId: 1,
-          createdAt: "2019-07-30T16:25:58.629Z",
-          updatedAt: "2019-07-30T16:25:58.629Z"
-        }
-      }
-    ],
-    Comments: [
-      {
-        id: 1,
-        text: "Sed dolores labore.",
-        UserId: 2,
-        RestaurantId: 1,
-        createdAt: "2019-07-30T16:24:55.451Z",
-        updatedAt: "2019-07-30T16:24:55.451Z",
-        User: {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y",
-          isAdmin: false,
-          image: "https://i.imgur.com/Q14p2le.jpg",
-          createdAt: "2019-07-30T16:24:55.204Z",
-          updatedAt: "2019-09-09T05:34:35.986Z"
-        }
-      },
-      {
-        id: 51,
-        text: "Quas accusamus sint blanditiis enim eveniet aliquam vel.",
-        UserId: 1,
-        RestaurantId: 1,
-        createdAt: "2019-07-30T16:24:55.452Z",
-        updatedAt: "2019-07-30T16:24:55.452Z",
-        User: {
-          id: 1,
-          name: "root test",
-          email: "root@example.com",
-          password:
-            "$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe",
-          isAdmin: true,
-          image: "https://i.imgur.com/58ImzMM.png",
-          createdAt: "2019-07-30T16:24:54.983Z",
-          updatedAt: "2019-09-15T07:49:43.737Z"
-        }
-      },
-      {
-        id: 101,
-        text: "Ipsa vitae voluptas.",
-        UserId: 2,
-        RestaurantId: 1,
-        createdAt: "2019-07-30T16:24:55.453Z",
-        updatedAt: "2019-07-30T16:24:55.453Z",
-        User: {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y",
-          isAdmin: false,
-          image: "https://i.imgur.com/Q14p2le.jpg",
-          createdAt: "2019-07-30T16:24:55.204Z",
-          updatedAt: "2019-09-09T05:34:35.986Z"
-        }
-      }
-    ]
-  },
-  isFavorited: false,
-  isLiked: true
-};
+import RestaurantDetail from './../components/RestaurantDetail'
+import RestaurantComments from './../components/RestaurantComments'
+import CreateComment from './../components/CreateComment'
+
+import restaurantsAPI from './../apis/restaurants'
+import { Toast } from './../utils/helpers'
+import { mapState } from 'vuex'
 
 export default {
   components: {
@@ -139,50 +32,70 @@ export default {
     return {
       restaurant: {
         id: -1,
-        name: "",
-        categoryName: "",
-        image: "",
-        openingHours: "",
-        tel: "",
-        address: "",
-        description: "",
+        name: '',
+        categoryName: '',
+        image: '',
+        openingHours: '',
+        tel: '',
+        address: '',
+        description: '',
         isFavorited: false,
         isLiked: false
       },
-      currentUser: dummyUser.currentUser,
       restaurantComments: []
-    };
+    }
   },
   created() {
-    const { id: restaurantId } = this.$route.params;
-    this.fetchRestaurant(restaurantId);
+    const { id: restaurantId } = this.$route.params
+    this.fetchRestaurant(restaurantId)
+  },
+  beforeRouteUpdate(to, from, next) {
+    const { id: restaurantId } = to.params
+    this.fetchRestaurant(restaurantId)
+    next()
+  },
+  computed: {
+    ...mapState(['currentUser'])
   },
   methods: {
-    fetchRestaurant(restaurantId) {
-      console.log("fetchRestaurant id: ", restaurantId);
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data, statusText } = await restaurantsAPI.getRestaurant({
+          restaurantId
+        })
 
-      this.restaurant = {
-        id: dummyData.restaurant.id,
-        name: dummyData.restaurant.name,
-        categoryName: dummyData.restaurant.Category.name,
-        image: dummyData.restaurant.image,
-        openingHours: dummyData.restaurant.opening_hours,
-        tel: dummyData.restaurant.tel,
-        address: dummyData.restaurant.address,
-        description: dummyData.restaurant.description,
-        isFavorited: dummyData.isFavorited,
-        isLiked: dummyData.isLiked
-      };
+        if (statusText !== 'OK') {
+          throw new Error(statusText)
+        }
 
-      this.restaurantComments = dummyData.restaurant.Comments;
+        this.restaurant = {
+          id: data.restaurant.id,
+          name: data.restaurant.name,
+          categoryName: data.restaurant.Category.name,
+          image: data.restaurant.image,
+          openingHours: data.restaurant.opening_hours,
+          tel: data.restaurant.tel,
+          address: data.restaurant.address,
+          description: data.restaurant.description,
+          isFavorited: data.isFavorited,
+          isLiked: data.isLiked
+        }
+
+        this.restaurantComments = data.restaurant.Comments
+      } catch (error) {
+        Toast.fire({
+          type: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
+      }
     },
     afterDeleteComment(commentId) {
       this.restaurantComments = this.restaurantComments.filter(
         comment => comment.id !== commentId
-      );
+      )
     },
     afterCreateComment(payload) {
-      const { commentId, restaurantId, text } = payload;
+      const { commentId, restaurantId, text } = payload
       this.restaurantComments.unshift({
         id: commentId,
         RestaurantId: restaurantId,
@@ -192,8 +105,9 @@ export default {
         },
         text,
         createdAt: new Date()
-      });
+      })
     }
   }
-};
-</script>>
+}
+</script>
+>
